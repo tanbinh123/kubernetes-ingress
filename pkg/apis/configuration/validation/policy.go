9baa51abec40ddb2ceb0c84efd07ae949807bfad
Haywood Shannon <5781935+haywoodsh@users.noreply.github.com>
2022-03-23 14:01:32 +0000

    Remove requirement of preview flag for policy

--- pkg/apis/configuration/validation/policy.go
+++ pkg/apis/configuration/validation/policy.go
@@ -15,11 +15,11 @@ import (
 
 // ValidatePolicy validates a Policy.
 func ValidatePolicy(policy *v1.Policy, isPlus, enablePreviewPolicies, enableAppProtect bool) error {
-	allErrs := validatePolicySpec(&policy.Spec, field.NewPath("spec"), isPlus, enablePreviewPolicies, enableAppProtect)
+	allErrs := validatePolicySpec(&policy.Spec, field.NewPath("spec"), isPlus, enableAppProtect)
 	return allErrs.ToAggregate()
 }
 
-func validatePolicySpec(spec *v1.PolicySpec, fieldPath *field.Path, isPlus, enablePreviewPolicies, enableAppProtect bool) field.ErrorList {
+func validatePolicySpec(spec *v1.PolicySpec, fieldPath *field.Path, isPlus, enableAppProtect bool) field.ErrorList {
 	allErrs := field.ErrorList{}
 
 	fieldCount := 0
@@ -30,19 +30,11 @@ func validatePolicySpec(spec *v1.PolicySpec, fieldPath *field.Path, isPlus, enab
 	}
 
 	if spec.RateLimit != nil {
-		if !enablePreviewPolicies {
-			return append(allErrs, field.Forbidden(fieldPath.Child("rateLimit"),
-				"rateLimit is a preview policy. Preview policies must be enabled to use via cli argument -enable-preview-policies"))
-		}
 		allErrs = append(allErrs, validateRateLimit(spec.RateLimit, fieldPath.Child("rateLimit"), isPlus)...)
 		fieldCount++
 	}
 
 	if spec.JWTAuth != nil {
-		if !enablePreviewPolicies {
-			allErrs = append(allErrs, field.Forbidden(fieldPath.Child("jwt"),
-				"jwt is a preview policy. Preview policies must be enabled to use via cli argument -enable-preview-policies"))
-		}
 		if !isPlus {
 			return append(allErrs, field.Forbidden(fieldPath.Child("jwt"), "jwt secrets are only supported in NGINX Plus"))
 		}
@@ -52,28 +44,16 @@ func validatePolicySpec(spec *v1.PolicySpec, fieldPath *field.Path, isPlus, enab
 	}
 
 	if spec.IngressMTLS != nil {
-		if !enablePreviewPolicies {
-			return append(allErrs, field.Forbidden(fieldPath.Child("ingressMTLS"),
-				"ingressMTLS is a preview policy. Preview policies must be enabled to use via cli argument -enable-preview-policies"))
-		}
 		allErrs = append(allErrs, validateIngressMTLS(spec.IngressMTLS, fieldPath.Child("ingressMTLS"))...)
 		fieldCount++
 	}
 
 	if spec.EgressMTLS != nil {
-		if !enablePreviewPolicies {
-			return append(allErrs, field.Forbidden(fieldPath.Child("egressMTLS"),
-				"egressMTLS is a preview policy. Preview policies must be enabled to use via cli argument -enable-preview-policies"))
-		}
 		allErrs = append(allErrs, validateEgressMTLS(spec.EgressMTLS, fieldPath.Child("egressMTLS"))...)
 		fieldCount++
 	}
 
 	if spec.OIDC != nil {
-		if !enablePreviewPolicies {
-			allErrs = append(allErrs, field.Forbidden(fieldPath.Child("oidc"),
-				"oidc is a preview policy. Preview policies must be enabled to use via cli argument -enable-preview-policies"))
-		}
 		if !isPlus {
 			return append(allErrs, field.Forbidden(fieldPath.Child("oidc"), "OIDC is only supported in NGINX Plus"))
 		}
--- pkg/apis/configuration/validation/policy_test.go
+++ pkg/apis/configuration/validation/policy_test.go
@@ -85,78 +85,6 @@ func TestValidatePolicy(t *testing.T) {
 			enableAppProtect:      true,
 			msg:                   "WAF policy with preview policies disabled",
 		},
-	}
-	for _, test := range tests {
-		err := ValidatePolicy(test.policy, test.isPlus, test.enablePreviewPolicies, test.enableAppProtect)
-		if err != nil {
-			t.Errorf("ValidatePolicy() returned error %v for valid input for the case of %v", err, test.msg)
-		}
-	}
-}
-
-func TestValidatePolicyFails(t *testing.T) {
-	t.Parallel()
-	tests := []struct {
-		policy                *v1.Policy
-		isPlus                bool
-		enablePreviewPolicies bool
-		enableAppProtect      bool
-		msg                   string
-	}{
-		{
-			policy: &v1.Policy{
-				Spec: v1.PolicySpec{},
-			},
-			isPlus:                false,
-			enablePreviewPolicies: false,
-			enableAppProtect:      false,
-			msg:                   "empty policy spec",
-		},
-		{
-			policy: &v1.Policy{
-				Spec: v1.PolicySpec{
-					AccessControl: &v1.AccessControl{
-						Allow: []string{"127.0.0.1"},
-					},
-					RateLimit: &v1.RateLimit{
-						Key:      "${uri}",
-						ZoneSize: "10M",
-						Rate:     "10r/s",
-					},
-				},
-			},
-			isPlus:                true,
-			enablePreviewPolicies: true,
-			enableAppProtect:      false,
-			msg:                   "multiple policies in spec",
-		},
-		{
-			policy: &v1.Policy{
-				Spec: v1.PolicySpec{
-					JWTAuth: &v1.JWTAuth{
-						Realm:  "My Product API",
-						Secret: "my-jwk",
-					},
-				},
-			},
-			isPlus:                false,
-			enablePreviewPolicies: true,
-			enableAppProtect:      false,
-			msg:                   "jwt(plus only) policy on OSS",
-		},
-		{
-			policy: &v1.Policy{
-				Spec: v1.PolicySpec{
-					WAF: &v1.WAF{
-						Enable: true,
-					},
-				},
-			},
-			isPlus:                false,
-			enablePreviewPolicies: true,
-			enableAppProtect:      false,
-			msg:                   "WAF(plus only) policy on OSS",
-		},
 		{
 			policy: &v1.Policy{
 				Spec: v1.PolicySpec{
@@ -228,6 +156,78 @@ func TestValidatePolicyFails(t *testing.T) {
 			isPlus:                true,
 			enablePreviewPolicies: false,
 			msg:                   "OIDC policy with preview policies disabled",
+		},
+	}
+	for _, test := range tests {
+		err := ValidatePolicy(test.policy, test.isPlus, test.enablePreviewPolicies, test.enableAppProtect)
+		if err != nil {
+			t.Errorf("ValidatePolicy() returned error %v for valid input for the case of %v", err, test.msg)
+		}
+	}
+}
+
+func TestValidatePolicyFails(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		policy                *v1.Policy
+		isPlus                bool
+		enablePreviewPolicies bool
+		enableAppProtect      bool
+		msg                   string
+	}{
+		{
+			policy: &v1.Policy{
+				Spec: v1.PolicySpec{},
+			},
+			isPlus:                false,
+			enablePreviewPolicies: false,
+			enableAppProtect:      false,
+			msg:                   "empty policy spec",
+		},
+		{
+			policy: &v1.Policy{
+				Spec: v1.PolicySpec{
+					AccessControl: &v1.AccessControl{
+						Allow: []string{"127.0.0.1"},
+					},
+					RateLimit: &v1.RateLimit{
+						Key:      "${uri}",
+						ZoneSize: "10M",
+						Rate:     "10r/s",
+					},
+				},
+			},
+			isPlus:                true,
+			enablePreviewPolicies: true,
+			enableAppProtect:      false,
+			msg:                   "multiple policies in spec",
+		},
+		{
+			policy: &v1.Policy{
+				Spec: v1.PolicySpec{
+					JWTAuth: &v1.JWTAuth{
+						Realm:  "My Product API",
+						Secret: "my-jwk",
+					},
+				},
+			},
+			isPlus:                false,
+			enablePreviewPolicies: true,
+			enableAppProtect:      false,
+			msg:                   "jwt(plus only) policy on OSS",
+		},
+		{
+			policy: &v1.Policy{
+				Spec: v1.PolicySpec{
+					WAF: &v1.WAF{
+						Enable: true,
+					},
+				},
+			},
+			isPlus:                false,
+			enablePreviewPolicies: true,
+			enableAppProtect:      false,
+			msg:                   "WAF(plus only) policy on OSS",
 		},
 		{
 			policy: &v1.Policy{
